79bc42539658b5127babe971cbcabc453f87eb94
pshen5282 <158219999+pshen5282@users.noreply.github.com>
2024-10-21 18:39:05 +0000

    Hello

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 document.addEventListener("DOMContentLoaded", () =>{
+    let chocolate = 0
+    let array = ["milk","dark","milk","milk","milk","milk","milk","milk"]
+    let selectedC = 0
+    let selectedCH = 1
+    let cDmg = 1
+    let nValAr = {
+        "milk": 1,
+        "dark": 5,
+    }
+    let nHpAr = {
+        "milk": 1,
+        "dark": 3,
+    }
+    document.getElementById("Button1").addEventListener("click", () =>{
+        selectedCH -= cDmg
+        if (selectedCH <= 0) {
+            chocolate += nValAr[array[selectedC]]
 
-    let ar = [1,1,1,1,1,1]
-window.console.log(document.getElementById("Button1"))
-document.getElementById("Button1").addEventListener("click", () => {
-    window.console.log(ar[1])
-    ar[1] += 1
-});
-
+            selectedC += 1
+            if (selectedC > 7) {
+                selectedC = 0
+            }
+            selectedCH = nValAr[array[selectedC]]
+        }
+        document.getElementById("text1").innerHTML = chocolate+""
+    })
 });
