f5f5bea32917869d20de6f5f6804cfb165497da2
pshen5282 <158219999+pshen5282@users.noreply.github.com>
2024-10-21 18:39:29 +0000

    Hi

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
+import * as THREE from 'three';
+
 document.addEventListener("DOMContentLoaded", () =>{
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
     let chocolate = 0
-    let array = ["milk","dark","milk","milk","milk","milk","milk","milk"]
+    let array = ["ruby","dark","milk","milk","milk","milk","milk","milk"]
     let selectedC = 0
     let selectedCH = 1
     let cDmg = 1
+    let cInv = {
+        "milk": 8,
+        "dark": 0,
+        "white": 0,
+        "ruby": 0.
+    }
     let nValAr = {
         "milk": 1,
-        "dark": 5,
+        "dark": 8,
+        "white": 1,
+        "ruby": 2,
     }
     let nHpAr = {
         "milk": 1,
-        "dark": 3,
+        "dark": 4,
+        "white": 1,
+        "ruby": 3,
     }
-    document.getElementById("Button1").addEventListener("click", () =>{
+    let nFlTxt = {
+        "milk": '\'standard milk chocolate\' (1/1), no special effects',
+        "dark": '\'a stronger tasting dark chocolate\' (8/4), no special effects',
+        "white": '\'white chocolate made from cocoa butter\' (1,1), nearby chocolate is worth 1.5x',
+        "ruby": '\'a more acidic chocolate between white and milk chocolate\'(2,3), when eaten bite strength +2'
+    }
+    let nStoContents = {
+        "BarOne": {
+            "dark": 34,
+            "white": 33,
+            "ruby": 33,
+        },
+    }
+    let bPrices = {
+        "BarOne": 50
+    }
+    document.getElementById("EatButton").addEventListener("click", () =>{
         selectedCH -= cDmg
         if (selectedCH <= 0) {
             chocolate += nValAr[array[selectedC]]
@@ -23,6 +58,6 @@ document.addEventListener("DOMContentLoaded", () =>{
             }
             selectedCH = nValAr[array[selectedC]]
         }
-        document.getElementById("text1").innerHTML = chocolate+""
+        document.getElementById("ChocolateDisplayText").innerHTML = chocolate+""
     })
 });
